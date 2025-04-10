92662bca32c12340d3213fd1b800301735f213d4
BossyRomain <romain.bossy.01@gmail.com>
2025-04-10 21:01:15 +0200

    (feat) Add main screen menu

--- super_tic_tac_toe/Scripts/game_over_menu.gd
+++ super_tic_tac_toe/Scripts/game_over_menu.gd
@@ -10,3 +10,7 @@ func show_draw() -> void:
 
 func _on_new_game_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/game_creation_menu.tscn")
+
+
+func _on_exit_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
