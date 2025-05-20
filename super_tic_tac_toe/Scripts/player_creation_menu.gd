1575d375e97148a449169744addf4388acd529fe
BossyRomain <romain.bossy.01@gmail.com>
2025-05-20 21:02:53 +0200

    (fix) Fix game creation menu

--- super_tic_tac_toe/Scripts/player_creation_menu.gd
+++ super_tic_tac_toe/Scripts/player_creation_menu.gd
@@ -5,16 +5,18 @@ signal name_changed(id, name)
 signal type_changed(id, type)
 
 @export var optional: bool = false
-@export var pawn_texture: Texture2D
 
 @onready var label: Label = $VBoxContainer/Label
 @onready var remove: Button = $VBoxContainer/Remove
 @onready var texture_rect: TextureRect = $VBoxContainer/TextureRect
 
+var pawn_texture: AtlasTexture
+
 var id: int = 0:
 	set(value):
 		id = value
 		_on_line_edit_text_changed("")
+		pawn_texture.region = Rect2i((id - 1) * 300, 0, 300, 300)
 
 func _ready() -> void:
 	if not optional:
@@ -22,6 +24,9 @@ func _ready() -> void:
 		remove.disabled = true
 	else:
 		visible = false
+	
+	pawn_texture = AtlasTexture.new()
+	pawn_texture.atlas = load("res://Assets/Textures/pawns.png")
 	texture_rect.texture = pawn_texture
 
 func _on_line_edit_text_changed(new_text: String) -> void:
